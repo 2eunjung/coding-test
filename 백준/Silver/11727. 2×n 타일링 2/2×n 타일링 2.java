3f0bfdeb2b1d6dfc765f601e731c6c950c4b151a
2eunjung <12103207@iscu.ac.kr>
2025-07-12 13:09:44 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        /*
+            2 * 1
+            2*1타일 하나의 타일로 채울 수 있음
+         */
+        int a = 1;
+        /*
+            2 * 2
+            2*1 - 2개 / 1*2 - 2개 / 2*2 - 1개
+            총 3개의 방법으로 타일을 채울 수 있음
+         */
+        int b = 3;
+        int result = N == 1 ? a : b;
+
+        // N이 1과 2였을 때의 값을 구해서 3부터 시작 => 3 이상인 경우 for문을 돌면서 값 도출
+        for (int i = 3; i <= N; i++) {
+            /*
+                점화식 코드
+                dp[n] = dp[n -1] + 2 * dp[n-2]
+                dp[n-1] = b
+                dp[n-2] = a
+                => b + 2 * a
+             */
+            result = (b + 2 * a) % 10007;
+            a = b;
+            b = result;
+        }
+        System.out.println(result);
+    }
+}
