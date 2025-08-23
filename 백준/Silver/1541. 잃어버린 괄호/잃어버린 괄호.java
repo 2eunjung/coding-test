f10130de156cbdb173a6c4fcc22771611c3e0ef7
2eunjung <12103207@iscu.ac.kr>
2025-08-23 13:53:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,56 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String st = br.readLine();
+        // 덧셈을 먼저 다 더한 후, 빼면 최솟값이 나옴
+        // 그래서 입력값을 먼저 -로 구분함
+        String[] st = br.readLine().split("-");
 
-        /*
-            입력값에 대한 조건을 정규식으로 해석
-                [0-9+-]+
-                    0부터 9까지의 숫자와 +기호, -기호를 정규식에서 허용한다는 의미
-                \\d+([+-]\\d+)*
-                    \\d         -> 하나 이상의 숫자로 맨 앞에 숫자가 와야함(기호 불가)
-                    ([+-]\\d+)* -> +숫자 혹은 -숫자가 0번 이상 반복됨
+        // - 기준으로 다 나누었으니 첫 그룹 집합은 다 더한 값을 result를 선언하면서 같이 값을 담음
+        int result = sum(st[0]);
 
-         */
-        if (st.matches("\\d+([+-]\\d+)*")){
-            // 입력값에 +나 -를 기준으로 숫자를 나눠서 배열에 담기
-            // split을 사용하면 기호가 버려지기 때문에 compile 사용
-            Pattern p = Pattern.compile("\\d+|[+-]");
-            Matcher m = p.matcher(st);
-
-            ArrayList<String> list = new ArrayList<>();
-
-            while (m.find()) {
-                list.add(m.group());
-            }
-
-            int result = 0;
-            boolean flag = false;
-
-            for (int i = 0; i < list.size(); i++) {
-                String token = list.get(i);
-
-                if (token.equals("-")){
-                    flag = true;
-                } else if (token.equals("+")){
-                    continue;
-                } else {
-                    int num = Integer.parseInt(token);
-                    if (flag){
-                        result -= num;
-                    } else {
-                        result += num;
-                    }
-                }
-            }
-            System.out.println(result);
+        for (int i = 1; i < st.length; i++) {
+            // 첫 그룹 집합외의 값은 다 빼기
+            result -= sum(st[i]);
         }
+
+        System.out.println(result);
+    }
+
+    private static int sum(String s) {
+        String[] st = s.split("\\+");
+        int sum = 0;
+        for(String num : st) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
     }
 }
